05642a953d6ae3cb92348918c18b0d0455b7d2b2
matthewfullhart <matthewfullhart@gmail.com>
2024-12-08 21:38:23 -0500

    finished model.py (I'm pretty sure?)

--- ml/model.py
+++ ml/model.py
@@ -119,11 +119,18 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
+    Slicedata = data.data.column_name == slice_value
+    
     X_slice, y_slice, _, _ = process_data(
         # your code here
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
-        
+        Slicedata,
+        categorical_features = categorical_features,
+        label = label,
+        training = False,
+        encoder = encoder,
+        lb = lb
     )
     preds = inference( model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
